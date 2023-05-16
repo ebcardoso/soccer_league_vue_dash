9e26b65e7498f3ce003d51e075cceb56ad16a937
Breno Cardoso <elisiobreno@gmail.com>
2023-05-16 00:30:36 -0300

    Component index page
    When we use it, it loads automatically the datatable

---
+++ src/components/index/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row g-3 mb-4 align-items-center justify-content-between">
+    <div class="col-auto">
+      <h1 class="app-page-title mb-0">{{titlePage}}</h1>
+    </div>
+  </div> <!--//row-->
+
+  <Datatable :datatable=datasource />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Datatable from "@/components/table/Datatable.vue"
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {
+    Datatable,  
+  },
+  props: {
+    viewmodel:Object,
+  },
+  data() {
+    return {
+      titlePage: this.viewmodel?.getTitleIndex(),
+      datasource: this.viewmodel?.getDatasource(),
+    }
+  }
+})
+</script>
