146dd7094201ddeea430d21eb8d3158209b0b060
Breno Cardoso <elisiobreno@gmail.com>
2023-06-13 00:38:40 -0300

    Datasource: Improving acess to access_token

--- src/boot/datasource.ts
+++ src/boot/datasource.ts
@@ -4,8 +4,10 @@ export const datasource = axios.create({
   baseURL: 'http://localhost:8000'
 });
 
-export const config = {
-  headers: {
-    Authorization: "Bearer " + localStorage.getItem('access_token')
+export const getConfigs = function() {
+  return {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem('access_token')
+    }
   }
 };
--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,22 +1,22 @@
-import { datasource, config } from '@/boot/datasource'
+import { datasource, getConfigs } from '@/boot/datasource'
 import { UserListDTO } from '@/dto/user/UserListDTO';
 import { User } from '@/models/User';
 
 export default {
   listAll:(search:string, page:number) => {
     const url = 'api/users/?page='+page+'&search='+search;
-    return datasource.get<UserListDTO>(url, config);
+    return datasource.get<UserListDTO>(url, getConfigs());
   },
   find:(id:string) => {
-    return datasource.get('api/users/'+id, config);
+    return datasource.get('api/users/'+id, getConfigs());
   },
   create:(user:User) => {
-    return datasource.post('api/users/', user, config);
+    return datasource.post('api/users/', user, getConfigs());
   },
   update:(user:User) => {
-    return datasource.patch('api/users/'+user.id+'/', user, config);
+    return datasource.patch('api/users/'+user.id+'/', user, getConfigs());
   },
   delete:(id:string) => {
-    return datasource.delete('api/users/'+id, config);
+    return datasource.delete('api/users/'+id, getConfigs());
   },
 }
