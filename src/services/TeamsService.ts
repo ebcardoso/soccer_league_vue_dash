ed9e8e5d91f62134d146f9251298f58bb56d03e2
Breno Cardoso <elisiobreno@gmail.com>
2023-06-13 00:44:15 -0300

    TeamsService: Fix api headers request

--- src/services/TeamsService.ts
+++ src/services/TeamsService.ts
@@ -1,22 +1,22 @@
-import { datasource, config } from '@/boot/datasource'
+import { datasource, getConfigs } from '@/boot/datasource'
 import { TeamListDTO } from '@/dto/team/TeamListDTO';
 import { Team } from '@/models/Team';
 
 export default {
   listAll:(search:string, page:number) => {
     const url = 'teams/?page='+page+'&search='+search;
-    return datasource.get<TeamListDTO>(url, config);
+    return datasource.get<TeamListDTO>(url, getConfigs());
   },
   find:(id:string) => {
-    return datasource.get('teams/'+id, config);
+    return datasource.get('teams/'+id, getConfigs());
   },
   create:(team:Team) => {
-    return datasource.post('teams/', team, config);
+    return datasource.post('teams/', team, getConfigs());
   },
   update:(team:Team) => {
-    return datasource.patch('teams/'+team.id, team, config);
+    return datasource.patch('teams/'+team.id, team, getConfigs());
   },
   delete:(id:string) => {
-    return datasource.delete('teams/'+id, config);
+    return datasource.delete('teams/'+id, getConfigs());
   }
 }
