f17cbb24702e068482b575975cd005b5855f87aa
Breno Cardoso <elisiobreno@gmail.com>
2023-07-02 00:30:54 -0300

    SelectField: Set values from API

--- src/components/form/SelectField.vue
+++ src/components/form/SelectField.vue
@@ -4,7 +4,7 @@
           :value="modelValue"
           @input="emitValue" 
           :required="fieldLocals?.required">
-    <option v-for="(option, index) in fieldLocals?.options" 
+    <option v-for="(option, index) in options" 
             :key="index" 
             :value="option[0]"
             :selected="isSelected(option[0])">
@@ -14,6 +14,7 @@
 </template>
 
 <script lang="ts">
+import { AxiosResponse } from 'axios';
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -23,6 +24,23 @@ export default defineComponent({
     modelValue:String
   },
   emits: ['update:inputSelectValue'],
+  data() {
+    return {
+      options: [] as Array<Array<any>>
+    }
+  },
+  mounted() {
+    if (this.fieldLocals?.fromApi==true) {
+      this.fieldLocals?.options.then((response:AxiosResponse) => {
+        var results = response.data.results;
+        results.forEach((value:any) => {
+          this.options.push([value?.id, value?.name]);
+        }); 
+      });
+    } else {
+      this.options = this.fieldLocals?.options;
+    }
+  },
   methods: {
     emitValue(e:any) {
       let value = e.target?.value;
